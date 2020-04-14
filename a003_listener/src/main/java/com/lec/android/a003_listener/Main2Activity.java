eb264ca51947d515b4bcb3ba987b2c03c55fdf98
ttngmai <frozenfruit24@gmail.com>
2020-04-14 21:49:23 +0900

    2020-04-14

---
+++ a003_listener/src/main/java/com/lec/android/a003_listener/Main2Activity.java
@@ -0,0 +1,15 @@
+package com.lec.android.a003_listener;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main2Activity extends AppCompatActivity {
+
+    // 과제 : 계산기 앱 만들기
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
